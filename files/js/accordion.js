552c23c9ac9c9fd3d87b08796f447f39cb1f045c
Christian Timbol <christiantimbol.email@gmail.com>
2017-08-26 12:46:28 -0700

    new profile pic! moved google-analytics to JS file instead of embedding on page. began plans to implement accordion function for #experience. fixed some broken image file paths.

---
+++ files/js/accordion.js
@@ -0,0 +1,17 @@
+/*** accordion.js: script for collapsing divs in #experience ***/
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
