7ca1748c02d4fbaaca384bafb6be0c072a4b80b3
Christian Timbol <christiantimbol.email@gmail.com>
2017-08-28 16:29:16 -0700

    added accordion function to #experience. other small visual changes as well

--- files/js/accordion.js
+++ files/js/accordion.js
@@ -1,17 +1,22 @@
 /*** accordion.js: script for collapsing divs in #experience ***/
 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(document).ready(function($) {
+    $('#accordion').find('.accordion-toggle').click(function(){
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  }
-}
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+
+      //Hide the other panels
+      $(".accordion-content").not($(this).next()).slideUp('fast');
+
+	  var el = $(this);
+	  if (el.text() == el.data("text-swap")) {
+	    el.text(el.data("text-original"));
+	  } else {
+	    el.data("text-original", el.text());
+	    el.text(el.data("text-swap"));
+	  }
+
+    });
+  });
